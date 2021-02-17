0715ff478021923790443eb93701b1f3b3cd10ce
vikram mahara <vmahara1999@gmail.com>
2021-02-17 20:50:10 +0530

    friends-request

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log(`Database connected to: ${conn.connection.host}`);
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+module.exports = connectDB;
